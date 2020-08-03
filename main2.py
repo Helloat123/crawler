85d506c4a806611c1f4246240187e3fe3bb8fb3d
Richard <zhu447136076@foxmail.com>
2020-08-03 17:41:43 +0800

    FINAL

--- main2.py
+++ main2.py
@@ -1,7 +1,29 @@
-import re
-def printinto(ss)
-	res=re.search('<div style=" float:left;width:50%;">.*</div> <div style=" float:left;width:50%;">',ss)
-	resstr=res.group(0)
-	reslog.write(resstr[36,-43]+'\n')
+import re,sys
+path=sys.argv[1]
 
-	res1=re.findall(ss,)
+inp=open(path,'r')
+s=inp.read()
+
+pos=s.find('学校名称：')
+if pos==-1: print("ERROR!!")
+i=pos+5
+cnt=0
+ss=''
+while s[i]!=' ':
+	cnt=cnt+1
+	ss=ss+s[i]
+	i=i+1
+name=ss
+
+pos=s.find('学校代码：')
+if pos==-1: print("ERROR!!")
+i=pos+5
+cnt=0
+ss=''
+while s[i]!=' ':
+	cnt=cnt+1
+	ss=ss+s[i]
+	i=i+1
+index=ss
+
+while 
